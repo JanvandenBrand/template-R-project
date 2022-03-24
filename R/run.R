0c0117696b21311c2cc832ab814cc9ea9bea26b4
JanvandenBrand <janvandenbrand@gmail.com>
2022-03-24 11:53:11 +0100

    add project scaffold

---
+++ R/run.R
@@ -0,0 +1,11 @@
+# Install packages
+source("R/env.R")
+
+# Extract, Transform and Load the Data
+source("R/etl.R")
+
+# Exploratory data analysis
+source("R/eda.R")
+
+# Perform in depth data analysis
+source("R/analysis.R")
